0d13f41b61bff8e1f76687724574f7ca64e68e6f
Rohan <31851961+rohanthakran@users.noreply.github.com>
2021-09-22 13:39:03 +0530

    We can store keep on google localstorage
    We can store keep on google localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import Header from './Header'
 import CreateNote from './CreateNote.jsx'
 import Note from './Note.jsx'
 
 const App =() =>{
     const [addItem,setAddItem] = useState([])
-    const addNote = (note) =>{
+    useEffect(() =>{
+          const locaTodos = localStorage.getItem("addItem");
+          if(locaTodos){
+              setAddItem(JSON.parse(locaTodos))
+          }  
+    },[])
+    const addNote = async note =>{
             setAddItem((prevData) =>{
                 return [...prevData,note];
             })
     }   
+    useEffect(() =>{
+        localStorage.setItem("addItem",JSON.stringify(addItem))
+    },[addItem])
+
     const onDelete = (id) =>{
         setAddItem((olddata) =>{
         return olddata.filter((curdata,indx) =>{
